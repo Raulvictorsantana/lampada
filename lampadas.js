2db3c8c13128f57f8d404bcd5b0864622d77add9
raul victor <raulvictormello@hotmail.com>
2021-10-17 20:22:09 -0300

    primeiro commit

---
+++ lampadas.js
@@ -0,0 +1,36 @@
+var ligada = document.getElementById('ligada');
+var desligado = document.getElementById ("desligado");
+var lampa = document.getElementById ("lampa");
+
+function fail () {
+    return lampa.src.indexof ('quebrada') > -1
+}
+ 
+function on(){
+
+    if(!fail () ){
+    lampa.src = "imagem/acessa.jpg"
+}
+}
+ligada.addEventListener('click', on );
+
+function off(){
+
+    if (!fail ()){
+    lampa.src = "imagem/apagada.jpg";
+    }
+
+}
+desligado.addEventListener('click', off);
+
+
+function qb () {
+    lampa.src = "imagem/quebrada.jpg";
+
+}
+lampa.addEventListener('dblclick', qb);
+
+lampa.addEventListener('mousemove', on);
+
+lampa.addEventListener('mouseleave', off);
+
